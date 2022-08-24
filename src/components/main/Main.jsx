e503e8ba7b7ffe6bd6ed618a282551782754f10c
HossamUK <hossam.m.pro@gmail.com>
2022-08-24 03:33:24 +0300

    update

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import HedersLabels from "../heders labels/HedersLabels";
 import LandPage from "../land page/LandPage";
+import Restaurants from "../restaurants/Restaurants";
 import "./main.css";
 
 export default function Main() {
   return (
     <div className="Main">
       <LandPage></LandPage>
-      <HedersLabels title={"Restaurants"}></HedersLabels>
+
+      <Restaurants></Restaurants>
     </div>
   );
 }
--- src/components/restaurants/Restaurants.jsx
+++ src/components/restaurants/Restaurants.jsx
@@ -1,6 +1,52 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./restaurants.css";
+import { BsArrowLeftShort, BsArrowRightShort } from "react-icons/bs";
+import HedersLabels from "../heders labels/HedersLabels";
 
 export default function Restaurants() {
-  return <div className="Restaurants"></div>;
+  const [restaurantsList, setRestaurantsList] = useState([
+    { name: "KFC", imgUrl: "/assets/restaurants/KFC.png" },
+    { name: "KFC", imgUrl: "/assets/restaurants/KFC.png" },
+    { name: "KFC", imgUrl: "/assets/restaurants/KFC.png" },
+    { name: "KFC", imgUrl: "/assets/restaurants/KFC.png" },
+    { name: "KFC", imgUrl: "/assets/restaurants/KFC.png" },
+    { name: "KFC", imgUrl: "/assets/restaurants/KFC.png" },
+    { name: "KFC", imgUrl: "/assets/restaurants/KFC.png" },
+    { name: "KFC", imgUrl: "/assets/restaurants/KFC.png" },
+    { name: "KFC", imgUrl: "/assets/restaurants/KFC.png" },
+    { name: "KFC", imgUrl: "/assets/restaurants/KFC.png" },
+    { name: "KFC", imgUrl: "/assets/restaurants/KFC.png" },
+  ]);
+
+  const ref = useRef(null);
+
+  const scroll = (scrollOffset) => {
+    ref.current.scrollLeft += scrollOffset;
+  };
+  const scrollLoop = () => {
+    console.log(ref.current.scrollLeft);
+    if (ref.current.scrollLeft > 484) {
+      ref.current.scrollTo(0, 0);
+    }
+  };
+  return (
+    <div className="Restaurants">
+      <div className="resturants_title">
+        <HedersLabels title={"Restaurants"}></HedersLabels>
+        <div className="resturants_buttons">
+          <button onClick={() => scroll(-200)}>
+            <BsArrowLeftShort />
+          </button>
+          <button onClick={() => scroll(200)}>
+            <BsArrowRightShort />
+          </button>
+        </div>
+      </div>
+      <div ref={ref} className="restaurants_contants" onScroll={scrollLoop}>
+        {restaurantsList.map((rest, ind) => {
+          return <img key={ind} src={rest.imgUrl} alt={rest.name} />;
+        })}
+      </div>
+    </div>
+  );
 }
